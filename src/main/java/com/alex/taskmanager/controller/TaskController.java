5ad8d3c874923e5330a5e12b6924ef91286a9146
Alex <pich.alex01@gmail.com>
2020-11-03 20:11:14 -0800

    Add some crud logic

--- src/main/java/com/alex/taskmanager/controller/TaskController.java
+++ src/main/java/com/alex/taskmanager/controller/TaskController.java
@@ -1,13 +1,66 @@
 package com.alex.taskmanager.controller;
 
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alex.taskmanager.exception.TaskCollectionException;
+import com.alex.taskmanager.model.Task;
 import com.alex.taskmanager.service.TaskService;
 
 @RestController
 public class TaskController {
-	
+
 	@Autowired
 	private TaskService taskService;
+
+	@PostMapping("/task")
+	public ResponseEntity<String> createTask(@RequestBody Task task) {
+		try {
+			taskService.createTask(task);
+			return new ResponseEntity("Successfully added task " + task.getTitle(), HttpStatus.OK);
+		} catch (ConstraintViolationException e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+	}
+
+	@GetMapping("/task")
+	public ResponseEntity<String> getAllTasks() {
+		List<Task> tasks = taskService.getAllTasks();
+		return new ResponseEntity(tasks, tasks.size() > 0 ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+	}
+
+	@DeleteMapping("/task/{id}")
+	public ResponseEntity deleteTaskById(@PathVariable("id") String id) {
+		try {
+			taskService.deleteTaskById(id);
+			return new ResponseEntity("Sucessfully deleted task with id " + id, HttpStatus.OK);
+		} catch (TaskCollectionException e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PutMapping("/task/{id}")
+	public ResponseEntity updateById(@PathVariable("id") String id, @RequestBody Task newTask) {
+		try {
+			taskService.updateTask(id, newTask);
+			return new ResponseEntity("Successfully updated task with id " + id, HttpStatus.OK);
+		} catch (ConstraintViolationException e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+		} catch (TaskCollectionException e) {
+			return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+
 }
--- src/main/java/com/alex/taskmanager/model/Task.java
+++ src/main/java/com/alex/taskmanager/model/Task.java
@@ -16,9 +16,60 @@ public class Task {
 
 	@NotNull(message = "Task status cannot be null")
 	private String status;
-	
+
 	@NotNull(message = "Task category cannot be null")
 	private String category;
+
+	public Task() {
+
+	}
+
+	public Task(String id, @NotNull(message = "Task title cannot be null") String title,
+			@NotNull(message = "Task status cannot be null") String status,
+			@NotNull(message = "Task category cannot be null") String category) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.status = status;
+		this.category = category;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [id=" + id + ", title=" + title + ", status=" + status + ", category=" + category + "]";
+	}
 
 //	TODO: Add date
 //	private Date dueDate; 
